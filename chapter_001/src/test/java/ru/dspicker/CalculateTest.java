9a15c70404d611a7ea464d14075df8c36552ee79
SpickerD <SpickerD@mail.ru>
2016-11-25 00:13:43 +0300

    add JUnit

--- chapter_001/src/test/java/ru/dspicker/CalculateTest.java
+++ chapter_001/src/test/java/ru/dspicker/CalculateTest.java
@@ -1,10 +1,15 @@
 package ru.dspicker;
 
 import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-public class CalculateTest{
+public class CalculateTest {
+	/**
+	* Test add.
+	*/
 
 	@Test
 	public void whenSetStopInEchoThenReturnThreeStops() {
@@ -18,6 +23,14 @@ public class CalculateTest{
 		Calculate calc = new Calculate();
 		String result = calc.echo(null);
 		assertThat(result, is("null null null"));
+	}
+	
+	@Test
+	public void whenAddOneToOneThenTwo() {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+		Calculate.main(null);
+		assertThat(out.toString(), is("Hello World\r\n"));
 	}
 
 }
