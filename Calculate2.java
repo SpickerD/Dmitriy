96952095f92be5a7c964ba221646241a0abde57d
SpickerD <SpickerD@mail.ru>
2017-02-04 00:22:17 +0300

    user input

--- Calculate2.java
+++ Calculate2.java
@@ -1,7 +1,26 @@
+/**
+	класс для вычисления суммы, вычитания, умножения, деления, возведения в степень двух чисел
+	Числа берутся из входящего массива.
+*/
+
 public class Calculate2 {
 	
 	public static void main(String[] arg){
 		
 		System.out.println("Calculate...");
+		double first = Double.valueOf(arg[0]);
+		double second = Double.valueOf(arg[1]);
+		
+		double summ = first + second;
+		double substraction = first - second;
+		double multiplication = first * second;
+		double division = first / second;
+		double exponentiation = Math.pow(first, second);
+		System.out.println("Sum " + summ);
+		System.out.println("substraction " + substraction);
+		System.out.println("multiplication " + multiplication);
+		System.out.println("division " + division);
+		System.out.println("exponentiation " + exponentiation);
+				
 	}
 }
