c966b4ae96fe5ce1d8773ad6f3ea8540e724688b
SpickerD <SpickerD@mail.ru>
2017-02-05 20:16:00 +0300

    specifically for 2.3

--- Calculator.java
+++ Calculator.java
@@ -6,16 +6,32 @@ public class Calculator {
 	/**
 		Результат вычисления
 	*/
-	private int result;
+	private double result;
 	
 	/**
 		Суммирует аргументы.
 		@param params Аргументы суммирования.
 	*/
-	public void add(int ... params) {
-		for (Integer param : params) {
+	public void add(double ... params) {
+		for (Double param : params) {
 			this.result += param;
 		}
+	}
+	
+	public void substraction (double a, double b) {
+		this.result = a-b;  //метод для вычитания
+	}
+	
+	public void multiplication (double a, double b) {
+		this.result = a*b;  // метод для умножения
+	}
+	
+	public void division (double a, double b) {
+		this.result = a/b;  // метод для деления
+	}
+	
+	public void exponentiation (double a, double b) {
+		this.result = Math.pow(a, b);  // метод для деления
 	}
 	
 	/**
@@ -23,7 +39,7 @@ public class Calculator {
 		@return результат вычисления.
 	*/
 	
-	public int getResult() {
+	public double getResult() {
 		return this.result;
 	}
 	
--- InteractRunner.java
+++ InteractRunner.java
@@ -1,26 +1,57 @@
-import java.util.Scanner;
+import java.util.*;
 
 /**
-	класс дл€ запуска калькул€тора. ѕоддерживает ввод пользовател€.
+	класс дл€ запуска калькул€тора. поддерживает ввод пользователя.
 */
 
 public class InteractRunner {
 	
 	public static void main(String[] arg) {
 		Scanner reader = new Scanner(System.in);
+		reader.useLocale(Locale.US);
 		try {
 			Calculator calc = new Calculator();
+			calc.cleanResult();
+			double first;
+			String clsClean = "no";
 			String exit = "no";
+			String arithOperation;
 			while (!exit.equals("yes")) {
-				System.out.println("Enter first arg : ");
-				String first = reader.next();
+				if (calc.getResult() == 0) {  //провер€ет, содержит ли результат вещественное число
+					System.out.println("Enter first arg : ");
+					first = reader.nextDouble();}  //если не содержит (т.е. значение 0), первое число считываетс€ с клавиатуры
+				else {
+				first = calc.getResult();}  //если содержит, то первому числу присваиваетс€ значение результата
+				
+				do {
+				System.out.println("select the arithmetic operation: +, -, *, /, ^ ");
+				arithOperation = reader.next(); //считываетс€, выбираемая пользователем арифметическа€ операци€
+				} while (arithOperation == "+" || arithOperation == "-" || arithOperation == "*" || arithOperation == "/" || arithOperation == "^");
+				
 				System.out.println("Enter second arg : ");
-				String second = reader.next();
-				calc.add(Integer.valueOf(first), Integer.valueOf(second));
+				double second = reader.nextDouble(); //считывается второе число
+				
+				if (arithOperation.equals("+"))
+					calc.add(first, second); // выполняется сложение
+				if (arithOperation.equals("-"))
+					calc.substraction(first, second); //выполняется вычитание
+				if (arithOperation.equals("*"))
+					calc.multiplication(first, second);  //выполняется умножение
+				if (arithOperation.equals("/"))
+					calc.division(first, second);  //выполняется деление
+				if (arithOperation.equals("^"))
+					calc.exponentiation(first, second);  //выполняется возведение в степень
+
+					
 				System.out.println("Result : " + calc.getResult());
+				
+				System.out.println("Do you want to clean the Result:  yes/no");
+				clsClean = reader.next();  //пользователь указывает очистить результат или нет
+				if (!clsClean.equals("no")) {
 				calc.cleanResult();
 				System.out.println("Exit : yes/no");
-				exit = reader.next();
+				exit = reader.next();  //пользователь указывает выйти ли из программы
+				}
 			}
 		} finally {
 			reader.close();
